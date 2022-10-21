032b325fec8f933230256709e689ae1abfd7c8e7
abdoulilah06 <114869795+abdoulilah06@users.noreply.github.com>
2022-10-21 20:43:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function auth(){
+    var menu=document.getElementById("menu")
+    var nev=document.getElementById("nev1")
+    menu.classList.toggle("active")
+    nev1.classList.toggle("on")
+}
+function tot(){
+    var btn=document.getElementById("btn")
+    var navi=document.getElementById("navi")
+    var tet=document.getElementById("tet")
+    var tet1=document.getElementById("tet1")
+    var tet2=document.getElementById("tet2")
+    var all=document.getElementById("all")
+    var tite=document.getElementById("tite")
+    var logo1=document.getElementById("logo1")
+    var logo3=document.getElementById("logo3")
+    var logo2=document.getElementById("mass")
+
+    logo2.classList.toggle("anif")
+    logo3.classList.toggle("actif")
+    logo1.classList.toggle("lol")
+    tite.classList.toggle("ses")
+    all.classList.toggle("bob")
+    tet.classList.toggle("tet")
+    tet1.classList.toggle("tet")
+    tet2.classList.toggle("tet")
+    btn.classList.toggle("run")
+    navi.classList.toggle("run")
+}
